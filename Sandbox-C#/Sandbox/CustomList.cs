9ca915f5133adc6da1b83ec8819611bb4030d692
danamurphy48 <danamurphy48@gmail.com>
2020-04-27 13:49:29 -0500

    Initial

---
+++ Sandbox-C#/Sandbox/CustomList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    public class CustomList<T>
+    {
+        // member variables (HAS A)
+        private T[] items;
+        private T[] capacity;
+        private T[] count;
+
+        // constructor (SPAWNER)
+        public CustomList()
+        {
+            items = new T[4];
+        }
+
+        // member methods (CAN DO)
+        public void Add(T item)
+        {
+
+        }
+        public bool Remove(T item)
+        {
+            return false;
+        }
+    }
+}
